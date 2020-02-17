87bfb8d972d9322c752a4cd5bdd4ea4b7d500e63
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2020-02-17 11:14:14 +0700

    [Irvan] add router

---
+++ router/index.js
@@ -0,0 +1,27 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.json({
+        "success": true,
+        "message": "This is homepage"
+    })
+})
+
+router.use(notFound)
+router.use(errorHandler)
+
+function notFound(req, res, next) {
+    res.status(404)
+    const error = new Error("Page Not Found")
+    next(error)
+}
+
+function errorHandler(err, req, res, next) {
+    res.status(res.statusCode || 500)
+    const message = err.message || "Internal Server Error"
+    res.json({
+        "message": message
+    })
+}
+
+module.exports = router
