01b39381c5ee195e24949d69dcccabb3763931f7
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2020-02-22 23:39:43 +0700

    [Irvan] add middleware at Image and Podcast Router

--- router/ImageRouter.js
+++ router/ImageRouter.js
@@ -1,15 +1,16 @@
 const router = require('express').Router()
 const imageController = require('../controller/ImageController')
 const fileupload = require('express-fileupload')
+const { checkToken } = require('../middleware')
 router.use(fileupload())
 
 // POST upload image
-router.post('/upload/:id_post', imageController.uploadImage)
+router.post('/upload/:id_post', checkToken, imageController.uploadImage)
 
 // GET show image by post
 router.get('/:id_post', imageController.showImageByPost)
 
 // DELETE image by id
-router.delete('/:id', imageController.deleteImageById)
+router.delete('/:id', checkToken, imageController.deleteImageById)
 
 module.exports = router
--- router/PodcastRouter.js
+++ router/PodcastRouter.js
@@ -1,15 +1,16 @@
 const router = require('express').Router()
 const podcastController = require('../controller/PodcastController')
 const fileupload = require('express-fileupload')
+const { checkToken } = require('../middleware')
 router.use(fileupload())
 
 // POST upload podcast
-router.post('/upload/:id_post', podcastController.uploadPodcast)
+router.post('/upload/:id_post', checkToken, podcastController.uploadPodcast)
 
 // GET show podcast by post
 router.get('/:id_post', podcastController.showPodcastByPost)
 
 // DELETE podcast by id
-router.delete('/:id', podcastController.deletePodcastById)
+router.delete('/:id', checkToken, podcastController.deletePodcastById)
 
 module.exports = router
