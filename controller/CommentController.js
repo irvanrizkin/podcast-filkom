d2b1fad8c7189fdab1a06b7e01925f52b01e50ee
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2020-02-22 22:22:25 +0700

    [Irvan] add post check exist at postCommentOnPost

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -9,26 +9,24 @@ const getCommentByPost = async(req, res, next) => {
     })
 }
 
-const postCommentOnPost = (req, res, next) => {
+const postCommentOnPost = async(req, res, next) => {
     const id_post = req.params.id_post
-    const id_user = req.body.id_user
+    const id_user = req.user.id
     const content = req.body.content
-    db.query('insert into comments(id_post, id_user, content) values(?, ?, ?)', [id_post, id_user, content])
-        .then(() => {
-            res.json({
-                "success": true,
-                "id_post": id_post,
-                "id_user": id_user,
-                "content": content,
-                "message": "Comment Added"
-            })
+    const [rows] = await db.query('select * from posts where id = ?', [id_post])
+    if (rows.length > 0) {
+        db.query('insert into comments(id_post, id_user, content) values(?, ?, ?)', [id_post, id_user, content])
+        res.json({
+            "success": true,
+            "id_post": id_post,
+            "content": content,
+            "message": "Comment Added"
         })
-        .catch((err) => {
-            res.json({
-                "success": false,
-                "error": err
-            })
-        })
+    } else {
+        res.status(404)
+        const error = new Error("Post Not Found")
+        next(error)
+    }
 }
 
 const updateComment = async(req, res, next) => {
